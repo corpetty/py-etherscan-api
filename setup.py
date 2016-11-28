4ddbee8fe75f8df28eb73ee700b6ad9156557106
searchthel0ve <searchthel0ve@gmail.com>
2016-11-28 17:28:45 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='py_etherscan_api',
     version='0.6.0',
     packages=['examples', 'examples.stats', 'examples.tokens', 'examples.accounts', 'etherscan'],
-    url='https://github.com/corpetty/py-etherscan-api',
+    url='https://github.com/searchthel0ve/py-etherscan-api',
     license='MIT',
-    author='coreypetty',
-    author_email='corey.a.petty@gmail.com',
+    author='searchthel0ve',
+    author_email='searchthel0ve@gmail.com',
     description='Python Bindings to Etherscan.io API'
 )
