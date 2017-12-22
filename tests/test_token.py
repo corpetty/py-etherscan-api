a36885b66f6ca96f204f4994edb742c31334a246
Corey Petty <corey.a.petty@gmail.com>
2017-12-22 13:47:08 -0500

    updated Token tests

--- tests/test_token.py
+++ tests/test_token.py
@@ -2,17 +2,17 @@ import pytest
 
 from etherscan.tokens import Tokens
 
-DGD_TOKEN_SUPPLY = '1994946756800000'
-DGD_TOKEN_BALANCE = '212900000000000'
+ELCOIN_TOKEN_SUPPLY = '21265524714464'
+ELCOIN_TOKEN_BALANCE = "135499"
 AP_IKEY = 'YourAPIkey'
-TOKEN_NAME = 'DGD'
-ADDRESS = '0x4366ddc115d8cf213c564da36e64c8ebaa30cdbd'
+CONTRACT_ADDRESS = '0x57d90b64a1a57749b0f932f1a3395792e12e7055'
+ADDRESS = '0xe04f27eb70e025b78871a2ad7eabe85e61212761'
 API_KEY = 'YourAPIkey'
 
 def test_get_token_supply():
-    api = Tokens(tokenname=TOKEN_NAME, api_key=(API_KEY))
-    assert (api.get_total_supply() == DGD_TOKEN_SUPPLY)
+    api = Tokens(contract_address=CONTRACT_ADDRESS, api_key=(API_KEY))
+    assert (api.get_total_supply() == ELCOIN_TOKEN_SUPPLY)
 
 def test_get_token_balance():
-    api = Tokens(tokenname=TOKEN_NAME, api_key=API_KEY)
-    assert(api.get_token_balance(ADDRESS) == DGD_TOKEN_BALANCE)
+    api = Tokens(contract_address=CONTRACT_ADDRESS, api_key=API_KEY)
+    assert(api.get_token_balance(ADDRESS) == ELCOIN_TOKEN_BALANCE)
