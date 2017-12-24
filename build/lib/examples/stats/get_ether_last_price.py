a783f720a6175b227cdfe8dc3040ebad3022d79f
Corey Petty <corey.a.petty@gmail.com>
2017-12-24 09:30:28 -0500

    Deleting build files.

--- build/lib/examples/stats/get_ether_last_price.py
+++
@@ -1,9 +0,0 @@
-from etherscan.stats import Stats
-import json
-
-with open('../../api_key.json', mode='r') as key_file:
-    key = json.loads(key_file.read())['key']
-
-api = Stats(api_key=key)
-last_price = api.get_ether_last_price()
-print(last_price)
