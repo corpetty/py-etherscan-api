5db2cdf885c923c43c6a1b40721677811100137c
Richard Horrocks <richardjhorrocks@gmail.com>
2019-02-04 21:26:26 +0000

    Blocks module test and example files.

--- examples/blocks/get_block_reward.py
+++ examples/blocks/get_block_reward.py
@@ -5,5 +5,5 @@ with open('../../api_key.json', mode='r') as key_file:
     key = json.loads(key_file.read())['key']
 
 api = Blocks(api_key=key)
-reward = api.get_block_reward(5747732)
+reward = api.get_block_reward(2165403)
 print(reward)
